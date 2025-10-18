ad352f9a34e8486ff843c639d6df7fec4caaed13
Hi0s <tienkhanh3234@gmail.com>
2025-10-18 23:57:36 +0800

    - update code flutter

--- ipad_demo/lib/api_service.dart
+++ ipad_demo/lib/api_service.dart
@@ -1,16 +1,40 @@
 ﻿import 'dart:io';
-
-import 'package:http/http.dart';
+import 'dart:math';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
 
 class ApiService {
-  String path = "";
+  final storage = FlutterSecureStorage();
+  String url = "";
   var uploadUri;
   var getResultUri;
 
   ApiService() {
     if (Platform.isIOS) {
-      path = "https://jsonplaceholder.typicode.com/";
+      url = "https://jsonplaceholder.typicode.com/";
     }
-    uploadUri = Uri.parse('$path/api/');
+    uploadUri = Uri.parse('$url/api/uploadImage');
+    getResultUri = Uri.parse('$url/api/getResult');
+  }
+
+  Upload(Uint8List img) async {
+    try {
+      var token = await storage.read(key: 'token');
+      var request = new http.MultipartRequest('POST', uploadUri);
+      Map<String, String> headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Token $token',
+      };
+      request.headers.addAll(headers);
+      request.files.add(await http.MultipartFile.fromBytes("face image", img));
+
+      var response = await request.send();
+      final respStr = await response.stream.bytesToString();
+      return response;
+    } catch (e) {
+      return 0;
+    }
   }
 }
